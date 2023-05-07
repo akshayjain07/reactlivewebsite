ea278de167b5299e8e3ad170bcff1243aec3e8f4
akshayjain07 <akshay.070603@gmail.com>
2023-05-07 18:46:57 +0530

    Updates

---
+++ precache-manifest.22e9eb2e8f2b224d61212f7d6587263f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7dfa417f09992d46a8c",
+    "url": "/reactlivewebsite/static/css/main.11d8d31f.chunk.css"
+  },
+  {
+    "revision": "c7dfa417f09992d46a8c",
+    "url": "/reactlivewebsite/static/js/main.e310b83e.chunk.js"
+  },
+  {
+    "revision": "fc7a02b70078fa310107",
+    "url": "/reactlivewebsite/static/js/runtime~main.bd8a33d0.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/reactlivewebsite/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/reactlivewebsite/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "9769beae2b45b978a3eebe875e2c997b",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
